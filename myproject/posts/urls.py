6ce8af877e7f791c34ea918caff3abab70b86e95
AliMusa02 <alimusayev044@gmail.com>
2025-06-06 02:38:08 +0000

    slug created and post page template added

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.posts),
+app_name = 'posts'
 
+urlpatterns = [
+    path('', views.posts, name="list"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
 
+
+# Create your views here.
 
 def posts(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
