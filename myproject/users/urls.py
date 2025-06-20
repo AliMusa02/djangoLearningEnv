e1f2eff2772f0238ef2d8278f3d9e3e4f4b9363b
AliMusa02 <ali.musayev044@gmail.com>
2025-06-20 02:30:01 +0000

    login authentication is done

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.users, name="register")
+    path('register/', views.users, name="register"),
+    path('login/', views.login_users, name="login")
+
 ]
